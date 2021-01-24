a1bf093986f630c7d1436b6ce32877350c2ea2cb
Ryan <r.alcoran@gmail.com>
2021-01-24 11:11:16 -0500

    Adding solution for word counter

---
+++ 06_wc/wc.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Author : Ryan Alcoran <r.alcoran>
+Date   : 2021-01-24
+Purpose: Emulate wc (word count)
+"""
+
+import argparse
+import sys
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Emulate wc (wordc count)',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('file',
+                        metavar='FILE',
+                        nargs='*',
+                        help='Input file(s)',
+                        type=argparse.FileType('rt'),
+                        default=[sys.stdin])
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    total_lines = 0
+    total_words = 0
+    total_bytes = 0
+    for fh in args.file:
+        num_lines = 0
+        num_words = 0
+        num_bytes = 0
+        for line in fh:
+            num_lines += 1
+            num_words += len(line.split())
+            num_bytes += len(line)
+        total_lines += num_lines
+        total_words += num_words
+        total_bytes += num_bytes
+        print(f'{num_lines:8}{num_words:8}{num_bytes:8} {fh.name}')
+    if len(args.file) > 1:
+        print(f'{total_lines:8}{total_words:8}{total_bytes:8} total')
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
